0fa75a4e312d0462a006daa74b45d0249f25823d
nojo7 <130080355+nojo7@users.noreply.github.com>
2023-06-21 11:32:11 -0400

    Add messaging

---
+++ main.js
@@ -0,0 +1,26 @@
+const messages = {
+    encourage: [
+        'Look, you have one shot. One opportunity at this life.',
+        'Yesterday is the past. Seize everything you ever wanted.',
+        'Your moment is now. Capture it. Don\'t let it slip.',
+        'The world is yours for the taking.',
+        'This opportunity comes once in a lifetime.',
+        'Success is your only option. Failure is not.'
+    ],
+    inspire: [
+        'Believe in the power of your dreams and the strength of your determination.',
+        'With every step forward, you rewrite the story of your success.',
+        'Embrace challenges as stepping stones to greatness, and let your resilience be your guiding light.',
+        'You possess within you the untapped potential to achieve extraordinary things. Unleash it with unwavering faith.',
+        'In the realm of possibility, your dreams have no limits. So dare to dream big and chase them fearlessly.',
+        'The fire within you burns brighter than any obstacle you face. Let it ignite your passion and propel you towards greatness.'
+    ],
+    quote: [
+        'Every day is a new opportunity to reach that goal. - Rick Ross',
+        'We can\'t change the world unless we change ourselves. - Biggie Smalls',
+        'If it\'s flipping hamburgers at McDonald\'s, be the best hamburger flipper in the world. Whatever it is you do, you have to master your craft. - Snoop Dogg',
+        'The more time you spend contemplating what you should have done... you lose valuable time planning what you can and will do. - Lil Wayne',
+        'Sometimes a loss is the best thing that can happen. It teaches you what you should have done next time. - Snoop Dogg',
+        'A goal is just a dream with a deadline. - Drake'
+    ],
+};
